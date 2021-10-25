0d3364f96ff7a1ace7acf29df9d5ca5ca2fdbd53
wakataw <agungpratama1001@gmail.com>
2021-10-26 03:47:03 +0700

    add auth manager

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dienna',
+    version='0.1a',
+    packages=['dienna'],
+    url='https://github.com/wakataw/dienna-python',
+    license='MIT',
+    author='Agung Pratama',
+    author_email='prrtmgng@gmail.com',
+    description='Office Automation Python SDK'
+)
